975892ad10944ee7c944d1ca75c192878fb2d300
g-jensen <greg.micah.jensen@gmail.com>
2024-01-15 18:20:13 -0500

    euler1

---
+++ examples/euler1.cpp
@@ -0,0 +1,10 @@
+#include "../src/core.cpp"
+#include "../src/io.cpp"
+
+int divisible_by_3_or_5(int n) {
+  return n % 3 == 0 || n % 5 == 0;
+}
+
+int main() {
+  prn(apply(add<int>,filter(divisible_by_3_or_5,range(1,1000))));
+}
